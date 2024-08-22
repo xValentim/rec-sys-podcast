31be5cb5d407b6d8dd5edf51b7b746dc37df1a2c
xValentim <joaogvr@al.insper.edu.br>
2024-08-22 17:24:57 -0300

    Add: + Run.sh

--- app.py
+++ app.py
@@ -21,3 +21,7 @@ def query(input_content: InputContent):
         "results": output, 
         "message": "OK"
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=1414)
